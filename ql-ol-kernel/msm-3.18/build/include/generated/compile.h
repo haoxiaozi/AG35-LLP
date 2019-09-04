5e2fd12ec0e2f92a67851a613fa296533e6a5a37
linzi <415306041@qq.com>
2019-09-04 13:21:51 +0800

    first commit

---
+++ ql-ol-kernel/msm-3.18/build/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Tue Sep 3 09:29:52 CST 2019"
+#define LINUX_COMPILE_BY "llp"
+#define LINUX_COMPILE_HOST "llp"
+#define LINUX_COMPILER "gcc version 4.9.3 (GCC) "
